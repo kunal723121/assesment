0d5bf4d9fc31d8934b0bc15d8445406a19293027
kunal <kunalkumbhakar11@gmail.com>
2024-05-31 20:05:36 +0530

    db update

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,18 @@
 import Express  from "express";
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import signRouter from "./Router/signRouter.js";
+
+dotenv.config({'path':'./.env'})
+console.log(process.env.MONGO_URL)
 let app=Express()
 app.use(cors())
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 app.use('/user',signRouter)
 
-mongoose.connect("mongodb://localhost:27017/assesment").then(()=>{
+mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("running")
 }).catch(()=>{
     console.log("err")
